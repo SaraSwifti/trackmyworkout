4bab0173afea939cb10de21a7393d0f4c7d3f7cf
Sara Taylor <sara.swifti@gmail.com>
2021-04-07 01:07:51 -0400

    adding html routes

---
+++ routes/html.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const path = require("path");
+
+//grabbing the data from index
+router.get("/", (reg, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+//grabbing data from the exercise html
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+//grabbing data from the status html
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router
