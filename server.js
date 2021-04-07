85e72722ddfc7947895d013f663c6e0a098d2c4f
Sara Taylor <sara.swifti@gmail.com>
2021-04-06 23:06:17 -0400

    put catch errors on the routes

--- server.js
+++ server.js
@@ -30,59 +30,71 @@ app.use(express.static("public"));
 //adding routes to server.js example from actiity 11
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
-  });
-  
-  app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/exercise.html"));
-  });
-  app.post("/api/workouts", (req, res) => {
-    Workout.create({}).then(function (allWorkouts) {
-      res.json(allWorkouts);
-    });
-  });
-  
-  app.get("/api/workouts", (req, res) => {
-    Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration: {
-            $sum: "$exercises.duration",
-          },
-        },
-      },
-    ]).then(function (allWorkouts) {
-      res.json(allWorkouts);
-    });
-  });
-  
-  app.get("/api/workouts/range", (req, res) => {
-    Workout.aggregate([
-      {
-        $addFields: {
-          totalDuration: {
-            $sum: "$exercises.duration",
-          },
-        },
-      },
-    ])
-      .sort({ _id: -1 })
-      .limit(7)
-      .then(function (allWorkouts) {
-        res.json(allWorkouts);
-      });
-  });
+});
 
-  app.put("/api/workouts/:id", ({ body, params }, res) => {
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.post("/api/workouts", (req, res) => {
+    Workout.create({}).then(function (allWorkouts) {
+        res.json(allWorkouts);
+    })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration",
+                },
+            },
+        },
+    ]).then(function (allWorkouts) {
+        res.json(allWorkouts);
+    })
+     .catch(err => {
+            res.json(err);
+        });
+});
+app.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration",
+                },
+            },
+        },
+    ])
+        .sort({ _id: -1 })
+        .limit(7)
+        .then(function (allWorkouts) {
+            res.json(allWorkouts);
+        })
+         .catch(err => {
+            res.json(err);
+        });
+});
+
+app.put("/api/workouts/:id", ({ body, params }, res) => {
     Workout.findByIdAndUpdate(
-      params.id,
-      { $push: { exercises: body } },
-      { new: true, runValidators: true }
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
     ).then(function (allWorkouts) {
-      res.json(allWorkouts);
+        res.json(allWorkouts);
+    })
+    .catch(err => {
+        res.json(err);
     });
-  });
-  
+});
+
 //start server from activity 11
 app.listen(PORT, () => {
-    console.log('\x1b[36m%s\x1b[0m',`App running on http://localhost:${PORT}`);
-  });
+    console.log('\x1b[36m%s\x1b[0m', `App running on http://localhost:${PORT}`);
+});
