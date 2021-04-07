4ee5826605372a66f6d16ada9e3b06085f8c8ca5
Sara Taylor <sara.swifti@gmail.com>
2021-04-06 23:02:27 -0400

    added routes to server JS like activities do

--- server.js
+++ server.js
@@ -1,27 +1,88 @@
+//dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+//port
 const PORT = process.env.PORT || 3000;
 
-const app = express();
 const Workout = require("./models/workout");
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+//middleware morgan dependency
+const app = express();
+app.use(logger("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+//routes
+// app.use(require("./routes/api.js"));
+// app.use(require("./routes/html.js"));
+//adding routes to server.js example from actiity 11
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+  });
+  
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+  });
+  app.post("/api/workouts", (req, res) => {
+    Workout.create({}).then(function (allWorkouts) {
+      res.json(allWorkouts);
+    });
+  });
+  
+  app.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ]).then(function (allWorkouts) {
+      res.json(allWorkouts);
+    });
+  });
+  
+  app.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
+        },
+      },
+    ])
+      .sort({ _id: -1 })
+      .limit(7)
+      .then(function (allWorkouts) {
+        res.json(allWorkouts);
+      });
+  });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
-// routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
-
-//start server
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    ).then(function (allWorkouts) {
+      res.json(allWorkouts);
+    });
+  });
+  
+//start server from activity 11
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+    console.log('\x1b[36m%s\x1b[0m',`App running on http://localhost:${PORT}`);
+  });
